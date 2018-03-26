d9704d7a8bc2db33f8874ba888b900b1b6b6f681
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-03-26 22:40:15 +0200

    Create Footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer-copyright py-3 text-center">
+                <h3>&copy; 2018 Copyright: Paweł Wojtasiński</h3>
+                <p>Recruitment task</p>
+            </div>
+        )
+    }
+}
+export default Footer
