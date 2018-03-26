c0f77cf1e845ed9f9e3582fbd91f59b5bcf520bf
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-03-26 22:40:35 +0200

    Create NavigationBar

---
+++ app/components/NavigationBar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <Link className="navbar-brand" to="/">Recruitment App</Link>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+export default NavigationBar
